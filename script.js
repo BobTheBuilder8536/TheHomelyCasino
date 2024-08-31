6f85721ef95dd4b2a29f6048691c110e554afb30
BobTheBuilder8536 <72139415+BobTheBuilder8536@users.noreply.github.com>
2024-08-31 10:34:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+
+var playerNum = 0; //comes from order in which players login
+var chaalAmount = 1;
+var round = 0;
+var betInfo = []
+
+
+
+import data from './round.json' with { type: 'json' };
+var pData = data[playerNum];
+
+//
+
+
+
+
+function launch(){
+    
+}
+
+function chaal(){
+
+}
