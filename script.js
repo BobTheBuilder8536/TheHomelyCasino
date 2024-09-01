0d1ebab9aac69557e341155df117054c6c91e3c1
BobTheBuilder8536 <72139415+BobTheBuilder8536@users.noreply.github.com>
2024-09-01 14:18:47 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -10,24 +10,25 @@ appId: "1:781705704501:web:1a8d4c1a1e6a2cdd582d7c"
 
 const app = initializeApp(firebaseConfig);
 
-import {getDatabase, ref, child, get, set, update, remove} from "https://www.gstatic.com/firebasejs/10.13.1/firebase-database.js";
+import {getDatabase, ref,onValue, child, get, set, update, remove} from "https://www.gstatic.com/firebasejs/10.13.1/firebase-database.js";
 
 var pName = "Prateek";
 var matchData;
 const db = child(ref(getDatabase()),'Round/');
 
 
-db.on('child_changed',(snapshot) => {
-  console.log(snapshot.val());
+onValue(db,(snapshot) => {
+  console.log(snapshot.val()["RoundData"]);
 })
-get(db).then((snapshot) => {
-    if (snapshot.exists()) {
-      matchData = snapshot.val()["RoundData"];
-      console.log(matchData);
-    } else {
-      console.log("No data available");
-    }
-});
+
+// get(child(db, `Round/`)).then((snapshot) => {
+//     if (snapshot.exists()) {
+//       matchData = snapshot.val()["RoundData"];
+//       console.log(matchData);
+//     } else {
+//       console.log("No data available");
+//     }
+// });
 
 
 // function active(){
