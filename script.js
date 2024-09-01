14ce5c2da1f486cd60aa809169530f95c5152f8c
BobTheBuilder8536 <72139415+BobTheBuilder8536@users.noreply.github.com>
2024-09-02 04:59:00 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -12,167 +12,181 @@ const app = initializeApp(firebaseConfig);
 
 import {getDatabase, ref,onValue, child, get, set, update, remove} from "https://www.gstatic.com/firebasejs/10.13.1/firebase-database.js";
 
+var pName = "";
 
-function chaalSound() {
-    var audio = new Audio("chaal.mp3");
-    audio.play();
-  }
+document.getElementById("displayName").style.display = "none";
 
-
-
-var pName = "Akshat";
-const db = getDatabase();
-var playerID = 0;
-var chaalAmount = 0;
-var playerActive = false;
-var nextPlayer = [];
-var previousPlayer = [];
-var betInfo = [];
-var packActive = false;
-
-function activate(data){
-  betInfo = data[pName].bets;
-  chaalAmount = data["RoundData"].chaal;
-  playerID = data[pName].playerNum;
-  playerActive = true;
-  packActive = true;
-  
-  var nameList = data["RoundData"].names;
-  
-  while (true){
-    if ((data[nameList[(playerID == 0) ? playerID = (nameList.length-1) : --playerID]].packed) == false){
-      previousPlayer = [playerID,nameList[playerID]];
-      break;
-    }
-  }
-
-  playerID = data[pName].playerNum;
-  
-  while (true){
-    if ((data[nameList[(playerID == (nameList.length-1)) ? playerID = 0 : ++playerID]].packed) == false){
-      nextPlayer = [playerID,nameList[playerID]];
-      break;
-    }
-  }
-  
-  playerID = data[pName].playerNum;
-
-  // console.log("Previous : " + previousPlayer);
-  // console.log("Next : " + nextPlayer);
-  // console.log(playerActive);
-}
-
-function uiUpdate(){
-
-}
-
-function fbUpdate(amount){
-  update(ref(db,'Round/' + pName),{
-    bets: betInfo,
-    totalBet: betInfo.reduce((a, b) => a + b, 0)
-  });
-
-  update(ref(db,'Round/RoundData'),{
-    chaal: amount,
-    playingNum: nextPlayer[0]
-  });
-}
-
-function deactivate(){
-  playerActive = false;
-  packActive = false;
-}
-
-function playChaal(){
-  console.log("Chaal");
-  betInfo.push(chaalAmount);
-  fbUpdate(chaalAmount);
-  // deactivate();
-}
-
-function playDouble(){
-  console.log("Double");                                                                                                                                                                                                                                                                                                                                                                                          
-  chaalAmount *=2;
-  betInfo.push(chaalAmount);
-  fbUpdate(chaalAmount);
-}
-
-function playShow(){
-  betInfo.push(chaalAmount);
-  
-  // Get previous unpacked player
-  update(ref(db,'Round/' + pName),{
-    showing: true
-  });
-
-  update(ref(db,'Round/' + previousPlayer[1]),{
-    showing: true
-  });
-
-  console.log("Show");
-  fbUpdate(chaalAmount);
-  
-}
-
-function playPack(){
-  update(ref(db,'Round/' + previousPlayer[1]),{
-    showing: false
-  });
-
-  update(ref(db,'Round/' + pName),{
-    packed: true,
-    showing: false
-  });
-
-  update(ref(db,'Round/RoundData/'),{
-    playingNum: playerID + 1
-  })
-  
-  console.log("Pack");
-}
+document.getElementById("loginForm").addEventListener("submit",(event)=>{
+  event.preventDefault()
+  document.getElementById("displayName").innerText = document.getElementById("loginInput").value;
+  document.getElementById("loginDiv").style.display = "none";
+  document.getElementById("displayName").style.display = "flex";
+});
 
 
+if (pName != ""){
+  const db = getDatabase();
+  var playerID = 0;
+  var chaalAmount = 0;
+  var playerActive = false;
+  var nextPlayer = [];
+  var previousPlayer = [];
+  var betInfo = [];
+  var packActive = false;
 
-onValue(child(ref(db),'Round/'),(snapshot) => {
-  var dbSnap = snapshot.val();
-  
-  if ((dbSnap[pName].playerNum == dbSnap["RoundData"].playingNum) && (dbSnap[pName].packed == false) && (dbSnap[pName].showing == false)){
-    activate(dbSnap);
-    // console.log(playerActive);
-    console.log("You are playing");
-  } else if (dbSnap[pName].showing){
-    playerActive = false;
+  function activate(data){
+    betInfo = data[pName].bets;
+    chaalAmount = data["RoundData"].chaal;
+    playerID = data[pName].playerNum;
+    playerActive = true;
     packActive = true;
-    console.log("You are showing");
-
-  } else {
-    deactivate();
-    console.log("Someone Else is playing");
+    
+    var nameList = data["RoundData"].names;
+    
+    while (true){
+      if ((data[nameList[(playerID == 0) ? playerID = (nameList.length-1) : --playerID]].packed) == false){
+        previousPlayer = [playerID,nameList[playerID]];
+        break;
+      }
+    }
+    
+    playerID = data[pName].playerNum;
+    
+    while (true){
+      if ((data[nameList[(playerID == (nameList.length-1)) ? playerID = 0 : ++playerID]].packed) == false){
+        nextPlayer = [playerID,nameList[playerID]];
+        break;
+      }
+    }
+    
+    playerID = data[pName].playerNum;
+    
+    // console.log("Previous : " + previousPlayer);
+    // console.log("Next : " + nextPlayer);
+    // console.log(playerActive);
   }
-});
-
-
-document.getElementById("chaalBut").addEventListener("click", () => {
-  if (playerActive){
-    chaalSound();
-    playChaal();
+  
+  function uiUpdate(){
+    document.getElementById("cumBet").innerText = betInfo.reduce((a, b) => a + b, 0);
+    
   }
-});
-
-document.getElementById("doubleBut").addEventListener("click", () => {
-  if (playerActive){
-    playDouble();
+  
+  function fbUpdate(amount){
+    update(ref(db,'Round/' + pName),{
+      bets: betInfo,
+      totalBet: betInfo.reduce((a, b) => a + b, 0)
+    });
+    
+    update(ref(db,'Round/RoundData'),{
+      chaal: amount,
+      playingNum: nextPlayer[0]
+    });
   }
-});
 
-document.getElementById("showBut").addEventListener("click", () => {
-  if (playerActive){
-    playShow();
+  function deactivate(){
+    playerActive = false;
+    packActive = false;
   }
-});
 
-document.getElementById("packBut").addEventListener("click", () => {
-  if (packActive){
-    playPack();
+  function playChaal(){
+    console.log("Chaal");
+    betInfo.push(chaalAmount);
+    
+    uiUpdate();
+    fbUpdate(chaalAmount);
   }
-});
+
+  function playDouble(){
+    console.log("Double");                                                                                                                                                                                                                                                                                                                                                                                          
+    chaalAmount *=2;
+    betInfo.push(chaalAmount);
+    uiUpdate();
+    fbUpdate(chaalAmount);
+  }
+
+  function playShow(){
+    betInfo.push(chaalAmount);
+    
+    uiUpdate();
+    update(ref(db,'Round/' + pName),{
+      showing: true
+    });
+    
+    update(ref(db,'Round/' + previousPlayer[1]),{
+      showing: true
+    });
+    
+    console.log("Show");
+    fbUpdate(chaalAmount);
+    
+  }
+
+  function playPack(){
+    update(ref(db,'Round/' + previousPlayer[1]),{
+      showing: false
+    });
+    
+    update(ref(db,'Round/' + pName),{
+      packed: true,
+      showing: false
+    });
+
+    
+    update(ref(db,'Round/RoundData/'),{
+      playingNum: nextPlayer[0]
+    })
+    
+    console.log("Pack");
+  }
+
+  function chaalSound() {
+      var audio = new Audio("chaal.mp3");
+      audio.play();
+    }
+
+
+  onValue(child(ref(db),'Round/'),(snapshot) => {
+    var dbSnap = snapshot.val();
+    
+    if ((dbSnap[pName].playerNum == dbSnap["RoundData"].playingNum) && (dbSnap[pName].packed == false) && (dbSnap[pName].showing == false)){
+      activate(dbSnap);
+      // console.log(playerActive);
+      console.log("You are playing");
+    } else if (dbSnap[pName].showing){
+      playerActive = false;
+      packActive = true;
+      console.log("You are showing");
+
+    } else {
+
+      deactivate();
+      console.log("Someone Else is playing");
+    }
+  });
+
+
+  document.getElementById("chaalBut").addEventListener("click", () => {
+    if (playerActive){
+      chaalSound();
+      playChaal();
+    }
+  });
+
+  document.getElementById("doubleBut").addEventListener("click", () => {
+    if (playerActive){
+      playDouble();
+    }
+  });
+
+  document.getElementById("showBut").addEventListener("click", () => {
+    if (playerActive){
+      playShow();
+    }
+  });
+
+  document.getElementById("packBut").addEventListener("click", () => {
+    if (packActive){
+      playPack();
+    }
+  });
+}
