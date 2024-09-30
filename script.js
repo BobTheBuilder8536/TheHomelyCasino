3f19c2c9f94aeaa0439d72aa45dab60cfb966507
BobTheBuilder8536 <72139415+BobTheBuilder8536@users.noreply.github.com>
2024-09-30 14:55:28 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -16,6 +16,27 @@ const db = getDatabase();
 
 console.log("Connected to Database!");
 
+const pullToRefresh = document.querySelector('.pull-to-refresh');
+let touchstartY = 0;
+document.addEventListener('touchstart', e => {
+  touchstartY = e.touches[0].clientY;
+});
+document.addEventListener('touchmove', e => {
+  const touchY = e.touches[0].clientY;
+  const touchDiff = touchY - touchstartY;
+  if (touchDiff > 0 && window.scrollY === 0) {
+    pullToRefresh.classList.add('visible');
+    e.preventDefault();
+  }
+});
+document.addEventListener('touchend', e => {
+  if (pullToRefresh.classList.contains('visible')) {
+    pullToRefresh.classList.remove('visible');
+    location.reload();
+  }
+});
+
+
 var pName = "";
 
 function addUser(username){
